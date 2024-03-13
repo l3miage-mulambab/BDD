ffbfe249d9c5520070e93716eda3e20e16725ba1
l3miage-mulambab <blaste.mulamba@etu.univ-grenoble-alpes.fr>
2024-03-13 16:41:35 +0100

    Update exo2-TP1

--- server/src/main/java/fr/uga/l3miage/tp1/exo2/models/NetflixUserEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo2/models/NetflixUserEntity.java
@@ -5,9 +5,11 @@ import fr.uga.l3miage.tp1.exo2.enums.Sex;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "netflix_user")
 public class NetflixUserEntity {
     @Id
     private Integer id;

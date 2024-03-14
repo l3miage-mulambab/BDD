4cb74f8da9bfacb1a98d180e1d94106162c29070
l3miage-mulambab <blaste.mulamba@etu.univ-grenoble-alpes.fr>
2024-03-14 13:57:04 +0100

    Update exo3-TP1

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo3/models/ProductEntity.java
@@ -0,0 +1,17 @@
+package fr.uga.l3miage.tp1.exo3.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product_entity")
+public class ProductEntity {
+    @Id
+    @Column(length = 15)
+    private String barCodeNumber;
+    @Column
+    private String name;
+    @Column
+    private boolean consumable;
+    @ManyToOne
+    private BrandEntity brand;
+}
